d559e62d38418a80e7cea92f2b1be19acf8b6e82
nabilhesham <nabil.hesham29@gmail.com>
2024-10-26 03:50:09 +0300

    add allowed_hosts

--- beachfront_villas_padel_reservation/settings.py
+++ beachfront_villas_padel_reservation/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')  # Use a default for local dev
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
 
 
 # Application definition
