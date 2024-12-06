fb891678ee4279394be7502ccddb1a7e6bca173d
nabilhesham <nabil.hesham29@gmail.com>
2024-12-06 02:07:08 +0200

    - add feature to add players to match - add match details modal - add under_construction.html

--- home/urls.py
+++ home/urls.py
@@ -3,12 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Auth URLs
     path('login/', views.login_view, name='login'),
     path('change-password/', views.change_password, name='change-password'),
     path('logout/', views.logout_view, name='logout'),
 
 
+    # Views URLs
     path('', views.home_view, name='home'),
+    path('under-construction/', views.under_construction_view, name='under-construction'),
     path('calendar/', views.calendar_view, name='calendar'),
-    path('matches/', views.get_matches, name='get_matches'),
+
+    # Data URLs
+    path('api/matches/', views.get_matches, name='get-matches'),
+    path('api/toggle-player-reservation/', views.toggle_player_reservation, name='toggle-player-reservation'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -9,6 +9,10 @@ from django.contrib.auth.forms import PasswordChangeForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+
+# general Imports
+import json
 
 # App imports
 from .models import Match, Reservation
@@ -68,12 +72,16 @@ def logout_view(request):
 def home_view(request):
     return redirect('calendar')
 
+def under_construction_view(request):
+    return render(request, 'under_construction.html')
+
 @custom_login_required
 def calendar_view(request):
     return render(request, 'calendar.html', {'user': request.user})
 
 ############################# Data ##########################################
 
+@custom_login_required
 def get_matches(request):
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
@@ -108,10 +116,58 @@ def get_matches(request):
             matches.append({
                 'start_time': match.start_time.strftime('%Y-%m-%dT%H:%M:%S'),
                 'end_time': match.end_time.strftime('%Y-%m-%dT%H:%M:%S'),
-                'main_players': [user.username for user in match.main_players.all()],
-                'reserve_players': [user.username for user in match.reserve_players.all()],
+                'main_players': list(match.main_players.values_list('user__username', flat=True)),
+                'reserve_players': list(match.reserve_players.values_list('user__username', flat=True)),
             })
 
         current_time = end_time
 
     return JsonResponse({'matches': matches})
+
+
+
+@custom_login_required
+def toggle_player_reservation(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        match_start = data.get('match_start')
+        match_end = data.get('match_end')
+        player_type = data.get('player_type')
+        user = request.user
+
+        try:
+            match_start = datetime.strptime(match_start, '%Y-%m-%dT%H:%M:%S')
+            match_end = datetime.strptime(match_end, '%Y-%m-%dT%H:%M:%S')
+
+            # Set minutes, seconds, and microseconds to zero
+            match_start = match_start.replace(minute=0, second=0, microsecond=0)
+            match_end = match_end.replace(minute=0, second=0, microsecond=0)
+
+        except ValueError:
+            return JsonResponse({'error': 'Invalid date format'}, status=400)
+
+
+        # Find or create the match
+        match, created = Match.objects.get_or_create(start_time=match_start, end_time=match_end)
+
+        try:
+            # Check if the user already has a reservation
+            reservation = Reservation.objects.filter(user=user, match=match).first()
+            if reservation:
+                if reservation.player_type == player_type:
+                    reservation.delete()  # Remove reservation
+                else:
+                    reservation.player_type = player_type
+                    reservation.save()
+            else:
+                # Create a new reservation
+                Reservation.objects.create(user=user, match=match, player_type=player_type)
+
+            # Return updated player lists
+            return JsonResponse({
+                'main_players': list(match.main_players.values_list('user__username', flat=True)),
+                'reserve_players': list(match.reserve_players.values_list('user__username', flat=True)),
+            })
+
+        except ValidationError as e:
+            return JsonResponse({'error': str(e)}, status=400)
